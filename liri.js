0c24c94decf17265342931d4f47c7822d0673c6f
Broba_Fett <brandon.j.rosser@gmail.com>
2019-11-16 10:50:22 -0500

    cleanup spacing

--- liri.js
+++ liri.js
@@ -37,12 +37,13 @@ function searchSong() {
     if (err) {
       return console.log('Error: ' + err);
     }
-    console.log("--------- SPOTIFY THIS SONG ---------");
+    
+    console.log("------- SPOTIFY THIS SONG -------");
     console.log("Artist(s): " + data.tracks.items[0].artists[0].name);
     console.log("Song Name: " + data.tracks.items[0].name);
     console.log("Album Name: " + data.tracks.items[0].album.name);
     console.log("Preview link on Spotify: " + data.tracks.items[0].external_urls.spotify);
-    console.log("-------------------------------------");
+    console.log("--------------------------------");
   });
 
 }
@@ -54,20 +55,20 @@ function searchBand() {
   var queryUrl = "https://rest.bandsintown.com/artists/" + userChoice + "/events?app_id=codingbootcamp&limit=20";
   axios.get(queryUrl).then(
     function (response) {
-      console.log("--------- CONCERT THIS ----------");
+      console.log("------ CONCERT THIS ------");
       console.log(" ");
       console.log("Search: " + userChoice);
-      console.log("---------------------------------");
+      console.log("-----------------------------");
       for (i = 0; i < response.data.length; i++) {
 
-        
+
 
         //VENUES WITHOUT REGIONS
         if (response.data[i].venue.region === "") {
           console.log(response.data[i].venue.name);
           console.log(response.data[i].venue.city + ", " + response.data[i].venue.country);
           console.log(moment(response.data[i].datetime).format('MM-DD-YYYY'));
-          console.log("---------------------------------");
+          console.log("-----------------------------");
         }
 
 
@@ -76,27 +77,27 @@ function searchBand() {
           console.log(response.data[i].venue.name);
           console.log(response.data[i].venue.city + ", " + response.data[i].venue.region + ", " + response.data[i].venue.country);
           console.log(moment(response.data[i].datetime).format('MM-DD-YYYY'));
-          console.log("---------------------------------");
+          console.log("-----------------------------");
         }
       }
     })
     .catch(function (error) {
       if (error.response) {
-        
+
 
 
         // ERROR RESPONSES
-        console.log("---------------Data---------------");
+        console.log("-----------Data-----------");
         console.log(error.response.data);
-        console.log("---------------Status---------------");
+        console.log("-----------Status-----------");
         console.log(error.response.status);
-        console.log("---------------Status---------------");
+        console.log("-----------Status-----------");
         console.log(error.response.headers);
       } else if (error.request) {
-        
+
         console.log(error.request);
       } else {
-        
+
         console.log("Error", error.message);
       }
       console.log(error.config);
@@ -115,7 +116,7 @@ function searchMovie() {
   //OMDB API REQUESTS
   axios.get(queryUrl).then(
     function (response) {
-      console.log("--------- MOVIE THIS ---------");
+      console.log("------ MOVIE THIS ------");
       console.log("Movie Title: " + response.data.Title);
       console.log("The movie came out in: " + response.data.Year);
       console.log("IMDB Rating: " + response.data.imdbRating);
@@ -124,25 +125,25 @@ function searchMovie() {
       console.log("Language: " + response.data.Language);
       console.log("Plot: " + response.data.Plot);
       console.log("Main actors in the movie are: " + response.data.Actors);
-      console.log("------------------------------");
+      console.log("-------------------------");
     })
     .catch(function (error) {
       if (error.response) {
 
 
-        
+
         // ERROR RESPONSES
-        console.log("---------------Data---------------");
+        console.log("-----------Data-----------");
         console.log(error.response.data);
-        console.log("---------------Status---------------");
+        console.log("-----------Status-----------");
         console.log(error.response.status);
-        console.log("---------------Status---------------");
+        console.log("-----------Status-----------");
         console.log(error.response.headers);
       } else if (error.request) {
-        
+
         console.log(error.request);
       } else {
-        
+
         console.log("Error", error.message);
       }
       console.log(error.config);
